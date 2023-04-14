d7da0d2ccea38339de02c57471320dd834d10257
Avinash Prasad <avionmission@gmail.com>
2023-04-14 15:34:55 +0530

    React frontend setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          smartfeed.ai
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
