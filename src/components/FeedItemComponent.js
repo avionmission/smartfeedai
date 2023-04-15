2c3e1c4a4666f394fb6f6ef9f9b679fc4dc2df79
Siraj Alam <crajmusics@gmail.com>
2023-04-16 04:19:21 +0530

    Integration done

--- src/components/FeedItemComponent.js
+++ src/components/FeedItemComponent.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import '../App.css'
 
-function FeedItemComponent(props) {
+function FeedItemComponent(title) {
     return(
         <div className='item-container'>
-        <p className='title'>{props.title}</p>
-        <p className='url'>{props.url}</p>
-        <p className='gist'>Gist: {props.gist}</p>
+        <p className='title'>{title.title}</p>
+        <p className='gist'>{title.gist}</p>
         <div className='action-buttons'>
           <button className='read-full-button'>READ FULL</button>
           <button className='save-for-later'>SAVE FOR LATER</button>
